6d7d86db5dd520b00ebab85ae1a976eb9edfcc89
iampassos <78429887+iampassos@users.noreply.github.com>
2025-09-15 22:39:03 -0300

    initial commit

---
+++ src/br/edu/cs/poo/ac/ordem/entidades/Cliente.java
@@ -0,0 +1,46 @@
+package br.edu.cs.poo.ac.ordem.entidades;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Cliente {
+	private String cpfCnpj;
+	private String nome;
+	private Contato contato;
+	private LocalDate dataCadastro;
+
+	public Cliente(String cpfCnpj, String nome, Contato contato, LocalDate dataCadastro) {
+		this.cpfCnpj = cpfCnpj;
+		this.nome = nome;
+		this.contato = contato;
+		this.dataCadastro = dataCadastro;
+	}
+	
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Contato getContato() {
+		return contato;
+	}
+	
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+	
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+	
+	public int getIdadeCadastro() {
+		return Period.between(dataCadastro, LocalDate.now()).getYears();
+	}
+}
